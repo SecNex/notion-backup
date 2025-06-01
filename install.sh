019e473e57242ec81faa7cf4577256d78055ffbc
Björn Benouarets <bbenouarets@proton.me>
2025-06-01 15:56:48 +0200

    feat: Installation script

--- install.sh
+++ install.sh
@@ -30,22 +30,28 @@ echo "Checking if the FILTER is set in the docker-compose.build.yaml file..."
 if grep -q "<FILTER>" docker-compose.build.yaml; then
     echo "FILTER is not set in the docker-compose.build.yaml file"
     # Ask the user to set the FILTER
-    read -p "Enter the FILTER: " FILTER
-    if [ -z "$FILTER" ]; then
-        echo "FILTER is not set"
-        exit 1
-    fi
+    while true; do
+        echo -n "Enter the FILTER: "
+        read FILTER
+        if [ ! -z "$FILTER" ]; then
+            break
+        fi
+        echo "FILTER cannot be empty. Please try again."
+    done
     sed -i '' "s/<FILTER>/${FILTER}/g" docker-compose.build.yaml
 fi
 
 if grep -q "<NOTION_API_KEY>" docker-compose.build.yaml; then
     echo "NOTION_API_KEY is not set in the docker-compose.build.yaml file"
     # Ask the user to set the NOTION_API_KEY
-    read -p "Enter the NOTION_API_KEY: " NOTION_API_KEY
-    if [ -z "$NOTION_API_KEY" ]; then
-        echo "NOTION_API_KEY is not set"
-        exit 1
-    fi
+    while true; do
+        echo -n "Enter the NOTION_API_KEY: "
+        read NOTION_API_KEY
+        if [ ! -z "$NOTION_API_KEY" ]; then
+            break
+        fi
+        echo "NOTION_API_KEY cannot be empty. Please try again."
+    done
     sed -i '' "s/<NOTION_API_KEY>/${NOTION_API_KEY}/g" docker-compose.build.yaml
 fi
 
